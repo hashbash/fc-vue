79d59ee0a22301a4e00e3600d40498712a5449db
hashbash <aqec@ya.ru>
2019-10-11 03:39:49 +0300

    init

--- src/components/SlideGroup.vue
+++ src/components/SlideGroup.vue
@@ -67,6 +67,15 @@
                                 <!--                            >-->
                                 <!--                                <strong>{{ f.destination_city_name }}</strong>-->
                                 <!--                            </v-card-title>-->
+                                <template v-slot:placeholder>
+                                    <v-row
+                                            class="fill-height ma-0"
+                                            align="center"
+                                            justify="center"
+                                    >
+                                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                                    </v-row>
+                                </template>
                             </v-img>
                         </template>
 
