5677caf6f8fdc57cead8ce5ff1572b8cb1ec073a
hashbash <aqec@ya.ru>
2019-11-21 01:55:15 +0300

    road to map

--- src/components/InteractiveMap.vue
+++ src/components/InteractiveMap.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
-        <div>Test</div>
         <v-content>
                     <MglMap :accessToken="accessToken"
-                            class="w-full md:w-1/2 flex-grow h-full min-h-full"
                             :mapStyle.sync="mapStyle"
                             :scrollZoom="false"
                             :center="mapCenter"
@@ -27,10 +25,10 @@ import { MglMap } from "vue-mapbox";
         data() {
             return {
                 accessToken: 'pk.eyJ1IjoiYnJhaW40bW9ua2V5IiwiYSI6ImNrMzBnNWM2czBuemkzbXMwd24zYzA4cmYifQ.brnSEahYdRNXMhE54aXfwg', // your access token. Needed if you using Mapbox maps
-                // mapStyle: 'mapbox://styles/mapbox/streets-v11', // your map style
+                mapStyle: 'mapbox://styles/mapbox/light-v10', // your map style
                 defaultZoom: 1,
                 mapCenter: [-71.038887, 42.364506],
-                mapStyle: 'mapbox://styles/mapbox/light-v9',
+                // mapStyle: 'mapbox://styles/mapbox/light-v9',
                 markerColor: 'blue',
                 useCustomMarker: false,
                 buttonDisabled: false,
